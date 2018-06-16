b801254987dd06ab66abb059c4f48353764b7805
Kazuhiko Okumura <engkokmr@gmail.com>
2018-06-17 07:20:34 +0900

    動作コメントを追加した 動作確認のため最初のページタイトル出力を検索実行前に移動した キャプチャするために待機時間を追加した

--- src/main/java/selenium/SeleniumExample.java
+++ src/main/java/selenium/SeleniumExample.java
@@ -8,30 +8,41 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumExample {
-  public static void main(String[] args) {
+  
+  public static void main(String[] args) throws InterruptedException {
     
     // ChromeDriverまでのパスを設定する
     System.setProperty("webdriver.chrome.driver", "selenium/chromedriver/2.40/chromedriver");
+    
+    // Chromeドライバーインスタンスを作成する
     WebDriver driver = new ChromeDriver();
     
+    // Google.comにアクセスする
     driver.get("https://www.google.com");
     
-    WebElement element = driver.findElement(By.name("q"));
-    
-    element.sendKeys("Cheese!");
-    
-    element.submit();
-    
+    // 表示しているページのTitleを出力する
     System.out.println("Page title is: " + driver.getTitle());
     
-    (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-      public Boolean apply(WebDriver webDriver) {
-        return webDriver.getTitle().toLowerCase().startsWith("cheese!");
-      }
-    });
+    Thread.sleep(1000);
     
+    // 検索ワード入力エレメントに、検索ワードを渡して、submitする
+    WebElement searchElement = driver.findElement(By.name("q"));
+    searchElement.sendKeys("Cheese!");
+    
+    Thread.sleep(1000);
+    
+    searchElement.submit();
+    
+    // ページが更新するまで待つ（Timeoutは10秒)
+    new WebDriverWait(driver, 10)
+        .until((ExpectedCondition<Boolean>) webDriver -> webDriver.getTitle().toLowerCase().startsWith("cheese!"));
+    
+    // ページ更新後のTitleを出力する
     System.out.println("Page title is: " + driver.getTitle());
     
+    Thread.sleep(1000);
+    
+    // ブラウザーを閉じる
     driver.quit();
   }
 }
